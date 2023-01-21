422251baa6dedfa43e41f2e7a8cd9b95c0e2140d
yumkx10 <yuji.i7211@gmail.com>
2023-01-21 21:28:26 +0900

    PWのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,7 @@ class User < ApplicationRecord
 
     validates :birthday
   end
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
