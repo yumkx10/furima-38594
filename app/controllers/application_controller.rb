faa225ec453ab897fc6e789b23621c0a5b6b701e
yumkx10 <yuji.i7211@gmail.com>
2023-01-21 16:53:49 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
