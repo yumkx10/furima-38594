fc266439f3110f4918dad8b7b6f293d6ab2d166e
yumkx10 <yuji.i7211@gmail.com>
2023-01-27 22:36:51 +0900

    itemsテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  has_many :orders
+
   with_options presence: true do
     validates :nickname
 
