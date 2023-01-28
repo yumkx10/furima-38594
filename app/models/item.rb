4f295d4915533776d93272f0a05a70327104118c
yumkx10 <yuji.i7211@gmail.com>
2023-01-28 22:01:16 +0900

    誤って作成したarticleモデルを削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
-  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
 end
