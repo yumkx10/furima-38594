785bc2e9b728cfc3779ada3ca9efa64dd95dbdbc
yumkx10 <yuji.i7211@gmail.com>
2023-01-29 00:08:53 +0900

    出品商品情報をテーブルに保存する機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
   
   def index
-    @items = Item.all
+    @items = Item.all.order(created_at: "DESC")
   end
 
   def new
@@ -9,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -23,5 +24,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :price, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
   end
-  
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,10 +11,15 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_day
 
-  with_options presence :true do
-    validates :image, :name, :description  presence: true
+  with_options presence: true do
+    validates :image, :name, :description, :price
 
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 0, message: "can't be blank" } do
       validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id
     end
+
+    with_options format: { with: /\A[0-9]+\z/ } do
+      validates :price, numericality: { greater_than: 300, less_than: 9999999 }
+    end
+  end
 end
