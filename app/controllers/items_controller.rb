3763ca4852f08657cfa4492b49c83d5d9568d68c
yumkx10 <yuji.i7211@gmail.com>
2023-01-29 19:43:42 +0900

    items_controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
   
   def index
-    @items = Item.all.order(created_at: "DESC")
   end
 
   def new
