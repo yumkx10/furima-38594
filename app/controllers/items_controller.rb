aceb8a5f56e620f5c72f6b5087c3265f504d56e3
yumkx10 <yuji.i7211@gmail.com>
2023-02-04 19:39:01 +0900

    updateアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.order(created_at: "DESC")
@@ -29,6 +29,14 @@ class ItemsController < ApplicationController
 =end
     else
       redirect_to root_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
     end
   end
 
