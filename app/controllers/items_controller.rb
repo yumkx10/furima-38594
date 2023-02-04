0f0e8bba38aab5a71e3909cecca9755dca47374d
yumkx10 <yuji.i7211@gmail.com>
2023-02-04 18:37:31 +0900

    editアクションをコントローラーに定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit]
   
   def index
     @items = Item.order(created_at: "DESC")
@@ -19,14 +20,26 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
+  def edit
+    if current_user.id  == @item.user_id 
+=begin
+&& @item.order.nil?
+=end
+    else
+      redirect_to root_path
+    end
+  end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :price, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
