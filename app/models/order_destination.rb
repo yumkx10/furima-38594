d1df3403f8927d5dc96ba0d4c9c11316485ffb34
yumkx10 <yuji.i7211@gmail.com>
2023-02-18 20:36:11 +0900

    OrderDestinationクラスの定義

---
+++ app/models/order_destination.rb
@@ -0,0 +1,23 @@
+class OrderDestination
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :house_number , :building_name , :phone_number, :token
+
+  with_options presence: true do
+    # orderモデルのバリデーション
+    validates :user_id
+    validates :item_id
+    # destinationモデルのバリデーション
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    # トークンのバリデーション
+    validates :token
+    
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Destination.create(order_id: order.id, postcode: postcode, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
+  end
+  
+end
