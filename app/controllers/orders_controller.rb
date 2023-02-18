4044a0aa598e78883e35f685101c810e7fcbf9e9
yumkx10 <yuji.i7211@gmail.com>
2023-02-18 20:59:21 +0900

    購入機能実装のためのコントローラーの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,24 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @order_destination = OrderDestination.new
   end
 
   def create
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid?
+      @order_destination.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
 
 end
