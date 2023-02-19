1d4ff2b82194940176fa484b627073132e55b57f
yumkx10 <yuji.i7211@gmail.com>
2023-02-19 19:20:12 +0900

    電話番号のバリデーション修正

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -11,7 +11,7 @@ class OrderDestination
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :house_number
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
     # トークンのバリデーション
     validates :token
   end
